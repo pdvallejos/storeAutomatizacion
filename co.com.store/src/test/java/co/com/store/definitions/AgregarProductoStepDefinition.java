6b15211bfa4273cb6fa7fa262f05635722d2601b
Estefanía Cardona Castro <estefania.cardona@sofka.local>
2023-03-01 10:09:47 -0500

    Creación de runner y stepDefinition para feature agregar productos al carro

---
+++ co.com.store/src/test/java/co/com/store/definitions/AgregarProductoStepDefinition.java
@@ -0,0 +1,16 @@
+package co.com.store.definitions;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Entonces;
+
+public class AgregarProductoStepDefinition {
+
+    @Cuando("agregue un producto al carrito de compras")
+    public void agregarProductoCarritoCompras() {
+
+    }
+    @Entonces("podrá observar que se agrego correctamente al carrito de compras")
+    public void observarProductoAgregadoCarritoCompras() {
+
+    }
+}
